0e8e21bea89eb21dc985cae1d223f8d4f2d94ff9
livia.cordeiro <livia.cordeiro@tecnogroup.com.br>
2022-03-29 13:47:19 -0300

    Consulta simples com query methods Referências:

--- src/main/java/com/cordeirolivia/workshopmongo/resources/PostResource.java
+++ src/main/java/com/cordeirolivia/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.cordeirolivia.workshopmongo.resources;
 
 import com.cordeirolivia.workshopmongo.domain.Post;
+import com.cordeirolivia.workshopmongo.resources.util.URL;
 import com.cordeirolivia.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,6 +20,14 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 
 }
